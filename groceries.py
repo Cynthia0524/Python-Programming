6918825db17703c904610273813ea717293546e3
Cynthia Yin <my1553@nyu.edu>
2017-07-26 04:16:15 +0000

    Test

--- groceries.py
+++ groceries.py
@@ -1,4 +1,5 @@
 import code
+#The raw data of products
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
